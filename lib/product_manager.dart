592665272319467b94513c028ff96d140313ee49
Khiet Le <khiet.le@ehochef.com>
2018-08-25 21:06:47 +0100

    initialise StatefulWidget with a default value

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,19 +3,27 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
