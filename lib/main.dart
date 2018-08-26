657ae3408ef823819c2c59620f4bfa213e7ddb50
Khiet Le <khiet.le@ehochef.com>
2018-08-26 12:19:04 +0100

    debug with debugShowMaterialGrid (end of section 3)

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowMaterialGrid: true,
       theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
