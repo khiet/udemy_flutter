93c373165567e6c1909ff8d8c5a13c12476d6116
Khiet Le <khiet.le@ehochef.com>
2018-08-26 20:16:30 +0100

    return empty Container inside build if nothing needs rendering (end of section 5)

--- lib/products.dart
+++ lib/products.dart
@@ -17,16 +17,15 @@ class Products extends StatelessWidget {
   }
 
   Widget _buildProductList() {
-    Widget productCard = Center(
-      child: Text('No products found, please add some'),
-    );
+    Widget productCards = Container();
+
     if (products.length > 0) {
-      productCard = ListView.builder(
+      productCards = ListView.builder(
         itemBuilder: _buildProductItem,
         itemCount: products.length,
       );
     }
-    return productCard;
+    return productCards;
   }
 
   @override
