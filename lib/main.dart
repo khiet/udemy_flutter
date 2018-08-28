be50f4218190d50a885eb14b912502ea2a884446
Khiet Le <khiet.le@ehochef.com>
2018-08-28 09:06:54 +0100

    manage products in main to manage state, complete onGenerateRoute to pass products to ProductPage

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,32 @@ import 'package:flutter/material.dart';
 import './pages/auth.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
+import './pages/product.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Map<String, String>> _products = [];
+
+  void _addProduct(Map<String, String> product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  void _deleteProduct(int index) {
+    setState(() {
+      _products.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +38,8 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepPurple),
       // home: AuthPage(),
       routes: {
-        '/': (BuildContext context) => ProductsPage(),
+        '/': (BuildContext context) =>
+            ProductsPage(_products, _addProduct, _deleteProduct),
         '/admin': (BuildContext context) => ProductsAdminPage()
       },
       onGenerateRoute: (RouteSettings settings) {
@@ -27,9 +50,9 @@ class MyApp extends StatelessWidget {
 
         if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute(
-            builder: (BuildContext context) =>
-                ProductPage(products[index]['title'], products[index]['image']),
+          return MaterialPageRoute<bool>(
+            builder: (BuildContext context) => ProductPage(
+                _products[index]['title'], _products[index]['image']),
           );
         }
 
