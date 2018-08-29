cd9f95f2ea1c403a7e235ed64a4e49bf67276bcf
Khiet Le <khiet.le@ehochef.com>
2018-08-29 21:11:54 +0100

    add TextField for product title

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePage();
+  }
+}
+
+class _ProductCreatePage extends State<ProductCreatePage> {
+  String _titleValue = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return Center(
-                child: Text('This is a Modal!'),
-              );
-            },
-          );
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              _titleValue = value;
+            });
+          },
+        ),
+        Text(_titleValue),
+      ],
     );
   }
 }
