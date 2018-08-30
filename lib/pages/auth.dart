d8a539e021fcf109f80ce659c4a8e27f01aa729e
Khiet Le <khiet.le@ehochef.com>
2018-08-30 10:58:12 +0100

    add switch to auth page (end of section 7)

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,6 +10,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _emailValue;
   String _passwordValue;
+  bool _acceptTermsValue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +39,15 @@ class _AuthPageState extends State<AuthPage> {
                   _passwordValue = value;
                 });
               },
+            ),
+            SwitchListTile(
+              value: _acceptTermsValue,
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTermsValue = value;
+                });
+              },
+              title: Text('Accept Terms'),
             ),
             SizedBox(
               height: 10.0,
