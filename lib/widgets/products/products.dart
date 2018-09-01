449a9066773bcc5583db9118129d0a766c28857a
Khiet Le <khiet.le@ehochef.com>
2018-09-01 12:38:08 +0100

    extract price into price tag widget

--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './pages/product.dart';
+import './price_tag.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
@@ -27,21 +27,8 @@ class Products extends StatelessWidget {
                 SizedBox(
                   width: 8.0,
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 6.0,
-                    vertical: 2.5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).accentColor,
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  child: Text(
-                    '\$${products[index]['price'].toString()}',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
+                PriceTag(
+                  products[index]['price'].toString(),
                 ),
               ],
             ),
