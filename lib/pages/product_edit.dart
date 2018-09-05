e507776512d9167ef585a63af479677594a85106
Khiet Le <khiet.le@ehochef.com>
2018-09-05 17:20:52 +0100

    update product (end of section 9)

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -4,8 +4,10 @@ class ProductEditPage extends StatefulWidget {
   final Function addProduct;
   final Function updateProduct;
   final Map<String, dynamic> product;
+  final int productIndex;
 
-  ProductEditPage({this.addProduct, this.updateProduct, this.product});
+  ProductEditPage(
+      {this.addProduct, this.updateProduct, this.product, this.productIndex});
 
   @override
   State<StatefulWidget> createState() {
@@ -131,7 +133,12 @@ class _ProductCreatePageState extends State<ProductEditPage> {
     }
     _formKey.currentState.save();
 
-    widget.addProduct(_formData);
+    if (widget.product == null) {
+      widget.addProduct(_formData);
+    } else {
+      widget.updateProduct(widget.productIndex, _formData);
+    }
+
     Navigator.pushReplacementNamed(context, '/products');
   }
 }
