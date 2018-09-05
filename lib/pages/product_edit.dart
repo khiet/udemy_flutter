959919c7caff6ad23e6ca00e721d5cdf05c064dc
Khiet Le <khiet.le@ehochef.com>
2018-09-05 21:20:19 +0100

    refactor (end of section 10)

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -27,11 +27,24 @@ class _ProductCreatePageState extends State<ProductEditPage> {
 
   @override
   Widget build(BuildContext context) {
+    final Widget pageContent = _buildPageContent(context);
+
+    return (widget.product == null)
+        ? pageContent
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Edit product'),
+            ),
+            body: pageContent,
+          );
+  }
+
+  Widget _buildPageContent(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
-    final Widget pageContent = GestureDetector(
+    return GestureDetector(
       onTap: () {
         FocusScope.of(context).requestFocus(FocusNode());
       },
@@ -67,15 +80,6 @@ class _ProductCreatePageState extends State<ProductEditPage> {
         ),
       ),
     );
-
-    return (widget.product == null)
-        ? pageContent
-        : Scaffold(
-            appBar: AppBar(
-              title: Text('Edit product'),
-            ),
-            body: pageContent,
-          );
   }
 
   Widget _buildPriceTextField() {
