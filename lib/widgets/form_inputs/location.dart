c1c52e462ba88f872a79edd3cef440cff6bfb2c3
Khiet Le <khiet.le@ehochef.com>
2018-09-13 16:38:04 +0100

    render a static map

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -9,11 +9,13 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  Uri _staticMapUri;
   final FocusNode _addressInputFocusNode = FocusNode();
 
   @override
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
+    getStaticMap();
     super.initState();
   }
 
@@ -21,6 +23,25 @@ class _LocationInputState extends State<LocationInput> {
   void dispose() {
     _addressInputFocusNode.removeListener(_updateLocation);
     super.dispose();
+  }
+
+  void getStaticMap() {
+    final StaticMapProvider staticMapViewProvider =
+        StaticMapProvider('AIzaSyDc72LQ1Qsa72z6Xg8dWb59aCg9_jEIqwA');
+
+    final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
+      [
+        Marker('position', 'Position', 41.40338, 2.17403),
+      ],
+      center: Location(41.40338, 2.17403),
+      width: 500,
+      height: 300,
+      maptype: StaticMapViewType.roadmap,
+    );
+
+    setState(() {
+      _staticMapUri = staticMapUri;
+    });
   }
 
   void _updateLocation() {
@@ -33,7 +54,13 @@ class _LocationInputState extends State<LocationInput> {
       children: <Widget>[
         TextFormField(
           focusNode: _addressInputFocusNode,
-        )
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Image.network(
+          _staticMapUri.toString(),
+        ),
       ],
     );
   }
