ab5a9a2d29c2990e721e409a73b0cd08d228c7d9
Khiet Le <khiet.le@ehochef.com>
2018-09-17 17:29:55 +0100

    slide in/out confirm password field

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -22,6 +22,7 @@ class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
   final TextEditingController _passwordTextController = TextEditingController();
   AuthMode _authMode = AuthMode.Login;
   AnimationController _controller;
+  Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
@@ -30,6 +31,16 @@ class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
     _controller = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 400),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0.0, -2.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastOutSlowIn,
+      ),
     );
   }
 
@@ -197,18 +208,21 @@ class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
         parent: _controller,
         curve: Curves.easeIn,
       ),
-      child: TextFormField(
-        obscureText: true,
-        validator: (String value) {
-          if (_passwordTextController.text != value &&
-              _authMode == AuthMode.Signup) {
-            return 'Password do not match';
-          }
-        },
-        decoration: InputDecoration(
-          labelText: 'Confirm Password',
-          filled: true,
-          fillColor: Colors.white,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: TextFormField(
+          obscureText: true,
+          validator: (String value) {
+            if (_passwordTextController.text != value &&
+                _authMode == AuthMode.Signup) {
+              return 'Password do not match';
+            }
+          },
+          decoration: InputDecoration(
+            labelText: 'Confirm Password',
+            filled: true,
+            fillColor: Colors.white,
+          ),
         ),
       ),
     );
